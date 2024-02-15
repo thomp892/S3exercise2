6f7bfb491dac31726cfcfa443ff0eb53f1a891f0
Mikaela Thompson <111909261+thomp892@users.noreply.github.com>
2024-02-15 15:13:18 -0500

    Add files via upload

---
+++ FanControl.ino
@@ -0,0 +1,83 @@
+//#include <LiquidCrystal.h>
+
+int motorPin = 2;
+int bPin = 4; //input pin
+int buttonState = 0;
+int LED_PIN = 10;
+
+//Ultrasonic Pins
+int ultraEcho = 13;
+int ultraTrig = 12;
+
+//distance and duration
+float distance;
+long duration;
+
+//const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+//LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+void setup() {
+
+  //set pin modes
+  pinMode(bPin, INPUT);
+  pinMode(motorPin, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
+
+  pinMode(ultraEcho, INPUT); //echo is the input for reading the distance value
+  pinMode(ultraTrig, OUTPUT); //trig is the output
+  //start the serial
+  Serial.begin(9600);
+  Serial.println("serial has begun!");
+} 
+
+void loop() {
+  digitalWrite(ultraTrig, LOW);
+  delayMicroseconds(5);
+  digitalWrite(ultraTrig, HIGH);
+  delayMicroseconds(20);
+  digitalWrite(ultraTrig, LOW);
+
+  //read ultrasonic values
+  duration = pulseIn(ultraEcho, HIGH);
+  //convert duration to distance in centimeters
+  distance = duration*0.034/2;
+  Serial.print(distance);
+  Serial.println("cm");
+  delay(5);
+
+  //read value from button switch
+  buttonState = digitalRead(bPin);
+  //Serial.println(buttonState);
+
+  // if (buttonState == 1) {
+  //       //let fan run for few seconds while on high
+  //   digitalWrite(motorPin, HIGH);
+  //   //turn on LED
+  //   digitalWrite(LED_PIN, HIGH);
+  //   Serial.println("Fan is on");
+  //   delay(50);
+  // }
+  // //if the tilt value read is high, turn fan on for 3 sec
+  // else if (buttonState == 0) {
+  //   //turn off FAN
+  //   //turn off LED
+  //   digitalWrite(LED_PIN, LOW);
+  //   digitalWrite(motorPin, LOW);
+  //   Serial.println("Fan is off");
+  //   delay(50);
+  //  }  
+
+  //check for distance between 30.48 = 12 inches
+  //then turn on FAN and LED when hand is within required distance
+  if(distance >= 0 && distance <= 7.62) {
+    //turn fan and LED on for 5 seconds
+    digitalWrite(LED_PIN, HIGH);
+    digitalWrite(motorPin, HIGH);
+    delay(500);
+  } else {
+    //turn off LED & fan when hand is not in required distance
+    digitalWrite(LED_PIN, LOW);
+    digitalWrite(motorPin, LOW);
+    delay(500);
+  }
+}
